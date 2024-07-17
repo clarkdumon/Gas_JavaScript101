5d58551766e81e7e81e2142fe9a0ac23d2acbe59
Clark Laurent Dumon <clarklaurent.dumon@outlook.com>
2024-07-17 16:56:56 +0000

    good

--- Appscript/project_1/romanToNumber.js
+++ Appscript/project_1/romanToNumber.js
@@ -1,31 +1,31 @@
-// const romanDict = {
-//     M: 1000,
-//     CM: 900,
-//     D: 500,
-//     CD: 400,
-//     C: 100,
-//     XC: 90,
-//     L: 50,
-//     XL: 40,
-//     X: 10,
-//     IX: 9,
-//     V: 5,
-//     IV: 4,
-//     I: 1
-// };
+const romanDict = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+};
 
-// function romanToNumber(roman) {
+function romanToNumber(roman) {
 
-// let total = 0;
+let total = 0;
 
-// for(let i=0, i<roman.lenth,i++){
-//     if{romanDict[roman[i]]<romanDict[roman[i+1]]}
+for(let i=0, i<roman.lenth,i++){
+    if{romanDict[roman[i]]<romanDict[roman[i+1]]}
 
-//     total -= romanDict[roman[i]];
+    total -= romanDict[roman[i]];
 
-// }else{
-//     total +=romanDict[roman[i]];
-// }
-// return total;
+}else{
+    total +=romanDict[roman[i]];
+}
+return total;
 
-// }
+}
