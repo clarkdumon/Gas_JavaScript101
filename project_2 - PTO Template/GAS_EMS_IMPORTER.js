7e88f413ee5053f31a895252fe1e5f544077dd4d
Clark Laurent Dumon <clarklaurent.dumon@outlook.com>
2024-08-10 01:43:44 +0800

    Update GAS_EMS_IMPORTER.js
    GetEmail function

--- project_2 - PTO Template/GAS_EMS_IMPORTER.js
+++ project_2 - PTO Template/GAS_EMS_IMPORTER.js
@@ -31,3 +31,22 @@ function importApproved() {
   }
   }
 
+function getEmail() {
+  
+  const EMSTextrange = 'A2:K' + PTOEMS.getSheetByName('EMS').getLastRow()
+  const EMSrange = PTOEMS.getSheetByName('EMS').getRange(EMSTextrange).getDisplayValues()
+  //EMSrange[0][7] Agent Email
+  //EMSrange[0][8] Agent Email
+
+  for(let i = 0; i<EMSrange.length;i++){
+    if(EMSrange[i][7]==""){
+      PTOEMS.getSheetByName('EMS').getRange(i+2,8).setFormula('xlookup(A'+(i+2)+', IMPORTRANGE("1pAh3oiqV_VJVUsE1hBVwY4i2zIlTtSc4Tv1v3vdmiNo","Active!$D:$D"),IMPORTRANGE("1pAh3oiqV_VJVUsE1hBVwY4i2zIlTtSc4Tv1v3vdmiNo","Active!$Q:$Q"),,0,-1)')
+      PTOEMS.getSheetByName('EMS').getRange(i+2,9).setFormula('xlookup(E'+(i+2)+', IMPORTRANGE("1pAh3oiqV_VJVUsE1hBVwY4i2zIlTtSc4Tv1v3vdmiNo","Active!$H:$H"),IMPORTRANGE("1pAh3oiqV_VJVUsE1hBVwY4i2zIlTtSc4Tv1v3vdmiNo","Active!$Q:$Q"),,0,-1)')
+    }
+    
+  }
+
+  const getVals = PTOEMS.getSheetByName('EMS').getRange(EMSTextrange).getDisplayValues()
+  PTOEMS.getSheetByName('EMS').getRange(EMSTextrange).setValues(getVals)
+
+}
